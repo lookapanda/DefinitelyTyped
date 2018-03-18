81478d478ebdd54440e05419849b1ee90c5ec741
lookapanda <lookapanda@ify.re>
2018-03-18 22:24:19 +0100

    wip

--- types/airbnb-prop-types/airbnb-prop-types-tests.ts
+++ types/airbnb-prop-types/airbnb-prop-types-tests.ts
@@ -1,10 +1,12 @@
-import * as AirbnbPropTypes from "airbnb-prop-types";
+import { ReactInstance } from 'react';
+import * as AirbnbPropTypes from 'airbnb-prop-types';
 
 interface Props {
     and: any;
     between: any;
     childrenHavePropXorChildren: any;
     childrenOf: any;
+    childrenOfType: any;
     childrenSequenceOf: any;
     componentWithName: any;
     elementType: any;
@@ -28,24 +30,35 @@ interface Props {
     uniqueArrayOf: any;
     valuesOf: any;
     withShape: any;
-
 }
 
 const airbnbPropTypes: AirbnbPropTypes.ValidationMap<Props> = {
     and: AirbnbPropTypes.and([AirbnbPropTypes.explicitNull()]).isRequired,
     between: AirbnbPropTypes.between({ gt({ min }) { return min; }}).isRequired,
     childrenHavePropXorChildren: AirbnbPropTypes.childrenHavePropXorChildren('foo').isRequired,
+    childrenOf: AirbnbPropTypes.childrenOf(AirbnbPropTypes.explicitNull()).isRequired,
+    childrenOfType: AirbnbPropTypes.childrenOfType('*', 'div').isRequired,
+    childrenSequenceOf: AirbnbPropTypes.childrenSequenceOfValidator({ validator: AirbnbPropTypes.explicitNull(), min: 1, max: 2 }).isRequired,
+    componentWithName: AirbnbPropTypes.componentWithName('test').isRequired,
+    elementType: AirbnbPropTypes.elementType(Component).isRequired,
+    explicitNull: AirbnbPropTypes.explicitNull().isRequired,
+    forbidExtraProps: AirbnbPropTypes.forbidExtraProps({ test: AirbnbPropTypes.explicitNull() }).isRequired,
+    integer: AirbnbPropTypes.integer().isRequired,
+    keysOf: AirbnbPropTypes.keysOf(AirbnbPropTypes.explicitNull(), 'keysOf').isRequired,
+    mutuallyExclusiveProps: AirbnbPropTypes.mutuallyExclusiveProps(AirbnbPropTypes.explicitNull(), 'foo', 'bar').isRequired,
+    mutuallyExclusiveTrueProps: AirbnbPropTypes.mutuallyExclusiveTrueProps('foo', 'bar').isRequired,
+    nChildren: AirbnbPropTypes.nChildren(1, AirbnbPropTypes.explicitNull()).isRequired,
+    nonNegativeInteger: AirbnbPropTypes.nonNegativeInteger.isRequired,
+    nonNegativeNumber: AirbnbPropTypes.nonNegativeNumber().isRequired,
+    numericString: AirbnbPropTypes.numericString().isRequired,
+    object: AirbnbPropTypes.object().isRequired,
+    or: AirbnbPropTypes.or([AirbnbPropTypes.integer().isRequired, AirbnbPropTypes.object().isRequired], 'or').isRequired,
+    range: AirbnbPropTypes.range(1, 2).isRequired,
+    restrictedProp: AirbnbPropTypes.restrictedProp(() => 'Some message').isRequired,
+    sequenceOf: AirbnbPropTypes.sequenceOf({ validator: AirbnbPropTypes.explicitNull(), min: 1, max: 2 }).isRequired,
+    shape: AirbnbPropTypes.shape({ test: AirbnbPropTypes.explicitNull() }).isRequired,
+    uniqueArray: AirbnbPropTypes.uniqueArray().isRequired,
+    uniqueArrayOf: AirbnbPropTypes.uniqueArrayof(AirbnbPropTypes.explicitNull(), () => {}, 'test').isRequired,
+    valuesOf: AirbnbPropTypes.valuesOf(AirbnbPropTypes.explicitNull()).isRequired,
+    withShape: AirbnbPropTypes.withShape(AirbnbPropTypes.explicitNull(), { length: AirbnbPropTypes.range(1, 2) }).isRequired
 };
-
-// const propTypes: PropTypes.ValidationMap<Props> = {
-//     any: PropTypes.any.isRequired,
-//     array: PropTypes.array.isRequired,
-//     bool: PropTypes.bool.isRequired,
-//     func: PropTypes.func.isRequired,
-//     number: PropTypes.number.isRequired,
-//     object: PropTypes.object.isRequired,
-//     string: PropTypes.string.isRequired,
-//     symbol: PropTypes.symbol.isRequired,
-//     node: PropTypes.node.isRequired,
-//     element: PropTypes.element.isRequired
-// };
--- types/airbnb-prop-types/index.d.ts
+++ types/airbnb-prop-types/index.d.ts
@@ -1,9 +1,10 @@
 // Type definitions for airbnb-prop-types 2.8
-// Project: https://github.com/airbnb/prop-types
+// Project: https://github.com/airbnb/prop-types#readme
 // Definitions by: lookapanda <https://github.com/lookapanda>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.2
+// TypeScript Version: 2.6
 
+import { ReactInstance } from 'react';
 export type Validator<T> = (object: T, key: string, componentName: string, ...rest: any[]) => Error | null;
 
 export interface Requireable<T> extends Validator<T> {
@@ -61,29 +62,26 @@ export type BetweenOptions =
     BetweenOptionsGt |
     BetweenOptionsGte;
 
-
-
-export type ReactComponent = (...args: any[]) => any;
-
 export const nonNegativeInteger: Requireable<any>;
 export function and(types: Array<Validator<any>>, name?: string): Requireable<any>;
 export function between(options: BetweenOptions): Requireable<any>;
 export function childrenHavePropXorChildren(prop: string | symbol): Requireable<any>;
 export function childrenOf(propType: Validator<any>): Requireable<any>;
-export function childrenOfType(...types: Array<Validator<any>>): Requireable<any>;
+export function childrenOfType(...types: Array<'*' | ReactInstance>): Requireable<any>;
 export function childrenSequenceOfValidator(...specifiers: Array<Specifier<any>>): Requireable<any>;
 export function componentWithName(name: string): Requireable<any>;
-export function elementType(type: '*' | string | ReactComponent): Requireable<any>;
+export function elementType(type: '*' | string | Component): Requireable<any>;
 export function explicitNull(): Requireable<any>;
 export function forbidExtraProps(propTypes: ValidationMap<any>): Requireable<any>;
 export function integer(): Requireable<any>;
-export function keysOf(propType: Validator<any>, name: string): Requireable<any>;
+export function keysOf(propType: Validator<any>, name?: string): Requireable<any>;
 export function mutuallyExclusiveProps(propType: Validator<any>, ...exclusiveProps: string[]): Requireable<any>;
 export function mutuallyExclusiveTrueProps(...exclusiveProps: string[]): Requireable<any>;
 export function nChildren(n: number, propType: Validator<any>): Requireable<any>;
+export function nonNegativeNumber(): Requireable<any>;
 export function numericString(): Requireable<any>;
 export function object(): Requireable<any>;
-export function or(validators: Array<Validator<any>>, name: string): Requireable<any>;
+export function or(validators: Array<Validator<any>>, name?: string): Requireable<any>;
 export function range(min: number, max: number): Requireable<any>;
 export function restrictedProp(messageFunction: (...args: any[]) => string | null): Requireable<any>;
 export function sequenceOf(...specifiers: Array<Specifier<any>>): Requireable<any>;
